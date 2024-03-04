f5139f25628b7cbc960c8c76c7b854240b3a2eb4
sbosse <sbosse@ovgu.de>
2024-03-04 11:12:24 +0100

    #19 publisher dois in init extended

--- output-api/src/services/init.service.ts
+++ output-api/src/services/init.service.ts
@@ -142,7 +142,8 @@ export class InitService {
         publ = await this.publisherRepository.save(publ);
         let alias = ["springer", "nature", "biomed central", "embo", "science china press", "verl. für sozialwissenschaften", "vieweg", "vs verlag für sozialwissenschaften"];
         await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
-        let doi_prefixes = ['10.1007', '10.1023', '10.1065', '10.1114', '10.1186', '10.1245', '10.1251', '10.1361', '10.1365', '10.1379', '10.1381', '10.1385', '10.1617', '10.1891', '10.3758', '10.4076', '10.4098', '10.4333', '10.5052', '10.5819', '10.7603'];
+        let doi_prefixes = ['10.1007', '10.1023', '10.1038', '10.1065', '10.1114', '10.1186', '10.1245', '10.1251', '10.1361', '10.1365', '10.1379', '10.1381', '10.1385', 
+        '10.1617', '10.1891', '10.3758', '10.4076', '10.4098', '10.4333', '10.5052', '10.5819', '10.7603'];
         await this.publisherDOIRepository.save(doi_prefixes.map(a => { return { publisher: publ, doi_prefix: a } }))
 
         publ = {
@@ -151,7 +152,8 @@ export class InitService {
         publ = await this.publisherRepository.save(publ);
         alias = ["wiley", "american geophysical union"];
         await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
-        doi_prefixes = ['10.1002'];
+        doi_prefixes = ['10.1002', '10.1034', '10.1046', '10.1111', '10.1113', '10.1196', '10.1256', '10.1301', '10.1348', '10.1359', '10.1506', '10.1516', 
+        '10.1526', '10.1576', '10.1581', '10.1892', '10.1897', '10.2746', '10.2755', '10.2966', '10.3162', '10.3170', '10.3322', '10.3401', '10.3405', '10.4004', '10.5054'];
         await this.publisherDOIRepository.save(doi_prefixes.map(a => { return { publisher: publ, doi_prefix: a } }))
 
         publ = {
@@ -160,6 +162,8 @@ export class InitService {
         publ = await this.publisherRepository.save(publ);
         alias = ["mdpi", "molecular diversity preservation international"];
         await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
+        doi_prefixes = ['10.3390']
+        await this.publisherDOIRepository.save(doi_prefixes.map(a => { return { publisher: publ, doi_prefix: a } }))
 
         publ = {
             label: 'IEEE',
@@ -185,13 +189,16 @@ export class InitService {
         publ = await this.publisherRepository.save(publ);
         alias = ["gruyter", "transcript"]
         await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
+        doi_prefixes = ['10.1515', '10.3884'];
+        await this.publisherDOIRepository.save(doi_prefixes.map(a => { return { publisher: publ, doi_prefix: a } }))
+
         publ = {
             label: 'Taylor & Francis',
         }
         publ = await this.publisherRepository.save(publ);
         alias = ["taylor", "routledge", "cass", "informa uk limited"]
         await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
-        doi_prefixes = ['10.1080'];
+        doi_prefixes = ['10.1080', '10.1531'];
         await this.publisherDOIRepository.save(doi_prefixes.map(a => { return { publisher: publ, doi_prefix: a } }))
 
         publ = {
@@ -200,6 +207,9 @@ export class InitService {
         publ = await this.publisherRepository.save(publ);
         alias = ["thieme"]
         await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
+        doi_prefixes = ['10.1055'];
+        await this.publisherDOIRepository.save(doi_prefixes.map(a => { return { publisher: publ, doi_prefix: a } }))
+
         publ = {
             label: 'Deutsche Gesellschaft für Akustik e.V. (DEGA)'
         }
@@ -218,18 +228,18 @@ export class InitService {
         publ = await this.publisherRepository.save(publ);
         alias = ["frontiers media", "frontiers research foundation"]
         await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
+        doi_prefixes = ['10.2741','10.3389','10.4175'];
+        await this.publisherDOIRepository.save(doi_prefixes.map(a => { return { publisher: publ, doi_prefix: a } }))
+
         publ = {
             label: 'Hogrefe'
         }
         publ = await this.publisherRepository.save(publ);
         alias = ["hogrefe"]
         await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
-        publ = {
-            label: 'Hogrefe'
-        }
-        publ = await this.publisherRepository.save(publ);
-        alias = ["hogrefe"]
-        await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
+        doi_prefixes = ['10.1024','10.1026','10.1027'];
+        await this.publisherDOIRepository.save(doi_prefixes.map(a => { return { publisher: publ, doi_prefix: a } }))
+        
         publ = {
             label: 'North-Holland'
         }
@@ -248,7 +258,7 @@ export class InitService {
         publ = await this.publisherRepository.save(publ);
         alias = ["oxford univ", "oup", "endocrine society"]
         await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
-        doi_prefixes = ['10.1093'];
+        doi_prefixes = ['10.1093','10.1112'];
         await this.publisherDOIRepository.save(doi_prefixes.map(a => { return { publisher: publ, doi_prefix: a } }))
 
         publ = {
@@ -257,6 +267,9 @@ export class InitService {
         publ = await this.publisherRepository.save(publ);
         alias = ["scitepress"]
         await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
+        doi_prefixes = ['10.5220'];
+        await this.publisherDOIRepository.save(doi_prefixes.map(a => { return { publisher: publ, doi_prefix: a } }))
+
         publ = {
             label: 'Stumpf & Kossendey'
         }
@@ -287,19 +300,26 @@ export class InitService {
         publ = await this.publisherRepository.save(publ);
         alias = ["world scientific pub"]
         await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
+        doi_prefixes = ['10.1142'];
+        await this.publisherDOIRepository.save(doi_prefixes.map(a => { return { publisher: publ, doi_prefix: a } }))
+
         publ = {
             label: 'OVGU'
         }
         publ = await this.publisherRepository.save(publ);
         alias = ["guericke", "universitätsbibliothek magdeburg"]
         await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
+        doi_prefixes = ['10.24352'];
+        await this.publisherDOIRepository.save(doi_prefixes.map(a => { return { publisher: publ, doi_prefix: a } }))
+
         publ = {
             label: 'Elsevier',
         }
         publ = await this.publisherRepository.save(publ);
         alias = ["elsevier", "academic press", "cell press", "churchill livingstone", "lancet publ", "science direct"]
         await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
-        doi_prefixes = ['10.1016'];
+        doi_prefixes = ['10.1006', '10.1016', '10.1053', '10.1054', '10.1067', '10.1078', '10.1157', '10.1197', '10.1205', '10.1240', '10.1367', '10.1383', 
+        '10.1529', '10.1580', '10.1602', '10.2353', '10.3182', '10.3816', '10.3921', '10.4065'];
         await this.publisherDOIRepository.save(doi_prefixes.map(a => { return { publisher: publ, doi_prefix: a } }))
 
         publ = {
@@ -308,41 +328,68 @@ export class InitService {
         publ = await this.publisherRepository.save(publ);
         alias = ["bmj p", "bmj journal"]
         await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
+        doi_prefixes = ['10.1136'];
+        await this.publisherDOIRepository.save(doi_prefixes.map(a => { return { publisher: publ, doi_prefix: a } }))
+
         publ = {
             label: 'Brill'
         }
         publ = await this.publisherRepository.save(publ);
         alias = ["böhlau", "vandenhoeck", "brill"]
         await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
+        doi_prefixes = ['10.1163', '10.2959'];
+        await this.publisherDOIRepository.save(doi_prefixes.map(a => { return { publisher: publ, doi_prefix: a } }))
+
         publ = {
             label: 'Wolters Kluwer'
         }
         publ = await this.publisherRepository.save(publ);
         alias = ["carl link", "wolters kluwer"]
         await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
-        publ = {
-            label: 'Deutsche Gesellschaft für Akustik e.V.'
-        }
-        publ = await this.publisherRepository.save(publ);
-        alias = ["deutsche gesellschaft für akustik"]
-        await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
-        publ = {
-            label: 'North-Holland'
-        }
-        publ = await this.publisherRepository.save(publ);
-        alias = ["north-holland"]
-        await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
+        doi_prefixes = ['10.1097', '10.1161', '10.1203', '10.1212', '10.1213', '10.1227', '10.1249', '10.1288', '10.1519', '10.2165', '10.2459', '10.7123'];
+        await this.publisherDOIRepository.save(doi_prefixes.map(a => { return { publisher: publ, doi_prefix: a } }))
+
         publ = {
             label: 'Cambridge University Press'
         }
         publ = await this.publisherRepository.save(publ);
         alias = ["cambridge univ", "cup"]
         await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
+        doi_prefixes = ['10.1017', '10.1375', '10.1557', '10.3815', '10.4039', '10.5948', '10.5949', '10.7135', '10.7313'];
+        await this.publisherDOIRepository.save(doi_prefixes.map(a => { return { publisher: publ, doi_prefix: a } }))
+
         publ = {
             label: 'Klett-Cotta'
         }
         publ = await this.publisherRepository.save(publ);
         alias = ["klett-cotta", "schattauer"]
         await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
+        
+        publ = {
+            label: 'JSTOR'
+        }
+        publ = await this.publisherRepository.save(publ);
+        alias = ["jstor", "journal storage"]
+        await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
+        doi_prefixes = ['10.2307'];
+        await this.publisherDOIRepository.save(doi_prefixes.map(a => { return { publisher: publ, doi_prefix: a } }))
+
+        publ = {
+            label: 'American Chemical Society (ACS)'
+        }
+        publ = await this.publisherRepository.save(publ);
+        alias = ["american chemical society"]
+        await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
+        doi_prefixes = ['10.1021'];
+        await this.publisherDOIRepository.save(doi_prefixes.map(a => { return { publisher: publ, doi_prefix: a } }))
+
+        publ = {
+            label: 'Sage Publications'
+        }
+        publ = await this.publisherRepository.save(publ);
+        alias = ["sage"]
+        await this.aliasPublRep.save(alias.map(a => { return { element: publ, alias: a } }))
+        doi_prefixes = ['10.1106', '10.1177', '10.1528', '10.1622', '10.1630', '10.2968', '10.3317', '10.4135', '10.4219'];
+        await this.publisherDOIRepository.save(doi_prefixes.map(a => { return { publisher: publ, doi_prefix: a } }))
     }
 }
