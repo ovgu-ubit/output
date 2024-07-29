f6e7cb54c6df51e376519c084e08c6e1775ac5b8
bossanova1986 <106057750+bossanova1986@users.noreply.github.com>
2024-07-29 09:14:38 +0200

    156 invoiceform bugs (#158)
    * table bugs
    
    * .
    
    * fixin'

--- output-api/src/main.ts
+++ output-api/src/main.ts
@@ -28,7 +28,7 @@ async function bootstrap() {
     const config = new DocumentBuilder()
         .setTitle('Output API')
         .setDescription('The Output API description')
-        .setVersion('0.2.5-beta')
+        .setVersion('0.2.6-beta')
         .addServer(base_path)
         .build();
     const options: SwaggerDocumentOptions = {
--- output-ui/src/app/app.component.ts
+++ output-ui/src/app/app.component.ts
@@ -11,7 +11,7 @@ import { AuthorizationService } from './security/authorization.service';
 })
 export class AppComponent implements OnInit, OnDestroy {
   public title: string = 'Output2';
-  public version = '0.2.5-beta';
+  public version = '0.2.6-beta';
   public user: string | null;
   public institution: string = '';
   public isLoading: boolean = true;
