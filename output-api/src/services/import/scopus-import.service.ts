0b018b5c8d370be7186cd645bdb335c55cc31fdb
bossanova1986 <106057750+bossanova1986@users.noreply.github.com>
2024-08-13 13:09:16 +0200

    aff tags matching and page count (#175)

--- output-api/src/services/import/scopus-import.service.ts
+++ output-api/src/services/import/scopus-import.service.ts
@@ -29,9 +29,9 @@ export class ScopusImportService extends ApiImportOffsetService {
     constructor(protected publicationService: PublicationService, protected authorService: AuthorService,
         protected geService: GreaterEntityService, protected funderService: FunderService, protected publicationTypeService: PublicationTypeService,
         protected publisherService: PublisherService, protected oaService: OACategoryService, protected contractService: ContractService,
-        protected costTypeService: CostTypeService, protected reportService: ReportItemService, protected instService:InstitutionService, protected languageService:LanguageService, protected configService: ConfigService,
+        protected costTypeService: CostTypeService, protected reportService: ReportItemService, protected instService: InstitutionService, protected languageService: LanguageService, protected configService: ConfigService,
         protected http: HttpService) {
-        super(publicationService, authorService, geService, funderService, publicationTypeService, publisherService, oaService, contractService, costTypeService, reportService,instService, languageService, configService, http);
+        super(publicationService, authorService, geService, funderService, publicationTypeService, publisherService, oaService, contractService, costTypeService, reportService, instService, languageService, configService, http);
         this.configService.get('searchTags').forEach(tag => {
             this.searchText += tag + " or "
         })
@@ -56,11 +56,11 @@ export class ScopusImportService extends ApiImportOffsetService {
         license: UpdateOptions.REPLACE_IF_EMPTY,
         invoice: UpdateOptions.IGNORE,
         status: UpdateOptions.REPLACE_IF_EMPTY,
-        editors :UpdateOptions.IGNORE,
-        abstract :UpdateOptions.REPLACE_IF_EMPTY,
-        citation :UpdateOptions.IGNORE,
-        page_count :UpdateOptions.REPLACE_IF_EMPTY,
-        peer_reviewed :UpdateOptions.IGNORE,
+        editors: UpdateOptions.IGNORE,
+        abstract: UpdateOptions.REPLACE_IF_EMPTY,
+        citation: UpdateOptions.IGNORE,
+        page_count: UpdateOptions.REPLACE_IF_EMPTY,
+        peer_reviewed: UpdateOptions.IGNORE,
     };
     protected url = 'https://api.elsevier.com/content/search/scopus?';
     protected max_res: number = 20;
@@ -76,12 +76,12 @@ export class ScopusImportService extends ApiImportOffsetService {
 
     setReportingYear(year: string) {
         this.params = [
-            { key: 'query', value: 'AFFIL('+this.searchText.slice(0, this.searchText.length - 4)+')+and+PUBYEAR+IS+'+year },
+            { key: 'query', value: 'AFFIL(' + this.searchText.slice(0, this.searchText.length - 4) + ')+and+PUBYEAR+IS+' + year },
             { key: 'view', value: 'complete' },
             { key: 'apiKey', value: this.configService.get('api_key_scopus') }]
     }
     protected importTest(element: any): boolean {
-        return element && element.affiliation?.find(aff => this.affiliationIncludesTags(aff.affilname));
+        return element && element.affiliation && this.affiliationIncludesTags(element.affiliation)
     }
 
     protected getDOI(element: any): string {
@@ -99,13 +99,26 @@ export class ScopusImportService extends ApiImportOffsetService {
     protected getInstAuthors(element: any): { first_name: string, last_name: string, orcid?: string, affiliation?: string }[] {
         if (!element.author || element.author.length === 0) return null;
         let aut_inst = [];
-        for (let i=0;i<element.author.length;i++) {
-            if (!element.affiliation[i]) continue;
-            if (this.affiliationIncludesTags(element.affiliation[i].affilname)) aut_inst.push(element.author[i])
+        for (let i = 0; i < element.author.length; i++) {
+            let aff = element.affiliation.filter(e => element.author[i].afid?.find(f => f['$'] === e.afid))
+            if (!aff || !Array.isArray(aff)) continue;
+            if (this.affiliationIncludesTags(aff)) aut_inst.push(element.author[i])
         }
         let res = [];
         for (let item of aut_inst) res.push({ last_name: item['surname'], first_name: item['given-name'] })
         return res;
+    }
+    private affiliationIncludesTags(affiliation: any[]): boolean {
+        for (let aff of affiliation) {
+            if (this.affiliationTagMatch(aff.affilname)) return true;
+        }
+        return false;
+    }
+    private affiliationTagMatch(affiliation: string): boolean {
+        for (let i = 0; i < this.configService.get('affiliationTags').length; i++) {
+            if (affiliation.toLowerCase().includes(this.configService.get('affiliationTags')[i])) return true;
+        }
+        return false;
     }
     protected getAuthors(element: any): string {
         let authors = element.author;
@@ -121,18 +134,11 @@ export class ScopusImportService extends ApiImportOffsetService {
         return result;
     }
 
-    private affiliationIncludesTags(affiliation): boolean {
-        for (let i = 0; i < this.configService.get('affiliationTags').length; i++) {
-            if (affiliation?.toLowerCase().includes(this.configService.get('affiliationTags')[i])) return true;
-        }
-        return false;
-    }
-
     protected getGreaterEntity(element: any): GreaterEntity {
         let identifiers = [];
-        if (element['prism:isbn']) identifiers = identifiers.concat(element['prism:isbn'].map(e => {return {type:'isbn',value:e['$']}}));
-        if (element['prism:eIssn']) identifiers.push({type:'issn',value:element['prism:eIssn'].slice(0,4)+'-'+element['prism:eIssn'].slice(4)});
-        if (element['prism:issn']) identifiers.push({type:'issn',value:element['prism:issn'].slice(0,4)+'-'+element['prism:issn'].slice(4)});
+        if (element['prism:isbn']) identifiers = identifiers.concat(element['prism:isbn'].map(e => { return { type: 'isbn', value: e['$'] } }));
+        if (element['prism:eIssn']) identifiers.push({ type: 'issn', value: element['prism:eIssn'].slice(0, 4) + '-' + element['prism:eIssn'].slice(4) });
+        if (element['prism:issn']) identifiers.push({ type: 'issn', value: element['prism:issn'].slice(0, 4) + '-' + element['prism:issn'].slice(4) });
         return {
             label: element['prism:publicationName'],
             identifiers
@@ -143,7 +149,7 @@ export class ScopusImportService extends ApiImportOffsetService {
     }
     protected getPubDate(element: any) {
         let split = element['prism:coverDate'].split('-');
-        return new Date(Date.UTC(split[0],split[1]-1,split[2]));
+        return new Date(Date.UTC(split[0], split[1] - 1, split[2]));
     }
     protected getLanguage(element: any): string {
         return null;
@@ -180,13 +186,14 @@ export class ScopusImportService extends ApiImportOffsetService {
         return element['dc:description'];
     }
     protected getCitation(element: any): string {
-        return 'Vol. '+element['prism:volume']+', Nr. '+element['prism:issueIdentifier']+', '+element['prism:pageRange'];
+        return 'Vol. ' + element['prism:volume'] + ', Nr. ' + element['prism:issueIdentifier'] + ', ' + element['prism:pageRange'];
     }
     protected getPageCount(element: any): number {
         try {
             let range = element['prism:pageRange']
             let split = range.split('-');
-            return Number(split[1])-Number(split[0])+1;
+            let res = Number(split[1]) - Number(split[0]) + 1;
+            if (!Number.isNaN(res) && res <= 2147483647) return res; else return null;//max int
         } catch (err) {
             return null;
         }
