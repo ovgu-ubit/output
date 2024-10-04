2526a29bdb3ec064018aac625920882d6a98c430
sbosse <sbosse@ovgu.de>
2024-10-04 10:32:54 +0200

    author export

---
+++ output-api/src/services/export/author-export.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
+import { AbstractExportService } from './abstract-export.service';
+import { PublicationService } from '../entities/publication.service';
+import { ReportItemService } from '../report-item.service';
+import { SearchFilter } from '../../../../output-interfaces/Config';
+import { Publication } from '../../entity/Publication';
+import { PublicationIndex } from '../../../../output-interfaces/PublicationIndex';
+import { AbstractFilterService } from '../filter/abstract-filter.service';
+import { ConfigService } from '@nestjs/config';
+import { AuthorService } from '../entities/author.service';
+
+@Injectable()
+/**
+ * abstract class for all exports
+ */
+export class AuthorExportService extends AbstractExportService {
+
+    quote = '"';
+    sep = ';';
+    df:Intl.DateTimeFormat;
+
+    constructor(private service:AuthorService, private reportService:ReportItemService, private configService:ConfigService) {
+        super(); 
+        this.df = new Intl.DateTimeFormat('de-DE');
+    }
+
+    protected name = 'Autor-Export';
+
+    public async export(filter?:{filter:SearchFilter, paths:string[]}, filterServices?:AbstractFilterService<PublicationIndex|Publication>[], by_user?: string) {
+        this.status_text = 'Started on ' + new Date();
+        this.report = this.reportService.createReport('Export',this.name, by_user);
+
+        let pubs = await this.service.get();
+
+        let res = "id;title;first_name;last_name;orcid;gnd_id;institutes\n";
+        for (let pub of pubs) {
+            res+=this.format(pub.id);
+            res+=this.format(pub.title);
+            res+=this.format(pub.first_name);
+            res+=this.format(pub.last_name);
+            res+=this.format(pub.orcid);
+            res+=this.format(pub.gnd_id);
+            res+=this.format(pub.institutes?.map(x => x.label).join(' | '));
+            res=res.slice(0,res.length-1);
+            res+='\n';
+        }
+        //res = res.replace(/undefined/g, '');
+        //finalize
+        this.progress = 0;
+        this.reportService.finish(this.report, {
+            status: 'Successfull export on ' + new Date(),
+            count_import: pubs.length
+        })
+        this.status_text = 'Successfull export on ' + new Date();
+        return res;
+    }
+
+    format(field):string {
+        let res = this.quote;
+        let value = field? (field.label? field.label : field) : '';
+        if (typeof value === 'string') res += value.replace(new RegExp(this.quote,"g"),"<quote>");
+        else if (value instanceof Date) res += this.df.format(value)
+        else if (Number.isNaN(value)) res += value.toLocaleString().slice(0,10000);
+        else res += value;
+        res += this.quote;
+        res += this.sep;
+        return res;
+    }
+}
