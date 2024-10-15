72b6002893f50a8c7bed00042ee21bbf3fd9fe70
bossanova1986 <sbosse@ovgu.de>
2024-10-15 09:03:57 +0200

    1.0.0-rc2

--- output-api/src/main.ts
+++ output-api/src/main.ts
@@ -28,7 +28,7 @@ async function bootstrap() {
     const config = new DocumentBuilder()
         .setTitle('Output API')
         .setDescription('The Output API description')
-        .setVersion('1.0.0-beta')
+        .setVersion('1.0.0-rc2')
         .addServer(base_path)
         .build();
     const options: SwaggerDocumentOptions = {
