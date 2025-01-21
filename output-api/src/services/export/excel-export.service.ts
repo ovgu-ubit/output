1556d1dac8e319762b483207e6ea13eff65609f6
bossanova1986 <sbosse@ovgu.de>
2025-01-21 15:38:28 +0100

    pubs to rows

--- output-api/src/services/export/excel-export.service.ts
+++ output-api/src/services/export/excel-export.service.ts
@@ -17,18 +17,18 @@ import * as XLSX from 'xlsx';
 export class ExcelExportService extends AbstractExportService {
 
     excel_response = true;
-    df:Intl.DateTimeFormat;
+    df: Intl.DateTimeFormat;
 
-    constructor(private publicationService:PublicationService, private reportService:ReportItemService, private configService:ConfigService, private invoiceService:InvoiceService) {
-        super(); 
+    constructor(private publicationService: PublicationService, private reportService: ReportItemService, private configService: ConfigService, private invoiceService: InvoiceService) {
+        super();
         this.df = new Intl.DateTimeFormat('de-DE');
     }
 
     protected name = 'Excel-Export';
 
-    public async export(filter?:{filter:SearchFilter, paths:string[]}, filterServices?:AbstractFilterService<PublicationIndex|Publication>[], by_user?: string) {
+    public async export(filter?: { filter: SearchFilter, paths: string[] }, filterServices?: AbstractFilterService<PublicationIndex | Publication>[], by_user?: string) {
         this.status_text = 'Started on ' + new Date();
-        this.report = this.reportService.createReport('Export',this.name, by_user);
+        this.report = this.reportService.createReport('Export', this.name, by_user);
 
         let pubs = await this.publicationService.getAll(filter?.filter);
         if (filter) for (let path of filter.paths) {
@@ -36,9 +36,50 @@ export class ExcelExportService extends AbstractExportService {
             if (so === -1) continue;
             pubs = await filterServices[so].filter(pubs) as Publication[]
         }
+        let cost_types = await this.invoiceService.getCostTypes();
+
+        let rows = [];
+        for (let pub of pubs) {
+            let row = {
+                id: pub.id,
+                locked: pub.locked,
+                status: pub.status,
+                title: pub.title,
+                doi: pub.doi,
+                link: pub.link,
+                authors: pub.authors,
+                authors_inst: pub.authorPublications?.map(e => { return e.author.last_name + ', ' + e.author.first_name }).join(' | '),
+                institutes: pub.authorPublications?.map(e => { return e.author.last_name + ', ' + e.author.first_name }).join(' | '),
+                corr_authors: pub.authorPublications?.filter(e => e.corresponding).map(e => { return e.author.last_name + ', ' + e.author.first_name }).join(' | '),
+                corr_institutes: pub.authorPublications?.filter(e => e.corresponding).map(e => { return e.institute?.label }).join(' | '),
+                greater_entity: pub.greater_entity?.label,
+                pub_date: pub.pub_date,
+                publisher: pub.publisher?.label,
+                language: pub.language?.label,
+                oa_category: pub.oa_category?.label,
+                pub_type: pub.pub_type?.label,
+                funders: pub.funders?.map(e => e.label).join(' | '),
+                contract: pub.contract?.label,
+                cost_approach: pub.cost_approach,
+                invoice_numbers: pub.invoices?.map(e => e.number).join(' | '),
+                net_costs: pub.invoices?.map(e => e.cost_items.map(e => e.euro_value).reduce((v, e) => v + e, 0)).reduce((v, e) => v + e, 0),
+                paid_amount: pub.invoices?.map(e => e.booking_amount).reduce((v, e) => v + e, 0),
+                cost_center: pub.invoices?.map(e => e.cost_center?.label).join(' | '),
+                data_source: pub.dataSource,
+                second_pub: pub.second_pub,
+                add_info: pub.add_info,
+                import_date: pub.import_date,
+                edit_date: pub.edit_date
+            }
+            if (pub.invoices.length > 0) for (let ct of cost_types) {
+                row[ct.label] = pub.invoices?.map(e => e.cost_items.filter(e => e.cost_type && e.cost_type.id === ct.id).map(e => e.euro_value + e.vat).reduce((v, e) => v + e, 0)).reduce((v, e) => v + e, 0)
+            }
+
+            rows.push(row);
+        }
 
         let workbook = XLSX.utils.book_new();
-        let worksheet = XLSX.utils.json_to_sheet(pubs)
+        let worksheet = XLSX.utils.json_to_sheet(rows)
         XLSX.utils.book_append_sheet(workbook, worksheet, "Publikationen");
 
         //finalize
@@ -48,6 +89,6 @@ export class ExcelExportService extends AbstractExportService {
             count_import: pubs.length
         })
         this.status_text = 'Successfull export on ' + new Date();
-        return XLSX.write(workbook, {type: "buffer", bookType: "xlsx"});
+        return XLSX.write(workbook, { type: "buffer", bookType: "xlsx" });
     }
 }
