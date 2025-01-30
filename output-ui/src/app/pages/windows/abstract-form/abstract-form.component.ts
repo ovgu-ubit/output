5bcd19523158a90b98b77bf5b5fd40ec9057197a
sbosse <sbosse@ovgu.de>
2025-01-30 13:40:05 +0100

    role + status

--- output-ui/src/app/pages/windows/abstract-form/abstract-form.component.ts
+++ output-ui/src/app/pages/windows/abstract-form/abstract-form.component.ts
@@ -1,11 +1,12 @@
 import { AfterViewInit, Component, Inject, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AuthorizationService } from 'src/app/security/authorization.service';
 import { ConfirmDialogComponent, ConfirmDialogModel } from 'src/app/tools/confirm-dialog/confirm-dialog.component';
 import { Entity } from '../../../../../../output-interfaces/Publication';
 import { EntityService } from 'src/app/interfaces/service';
+import { Observable, of, concatMap, map } from 'rxjs'
 
 @Component({
   selector: 'app-abstract-form',
@@ -18,9 +19,10 @@ export class AbstractFormComponent<T extends Entity> implements OnInit, AfterVie
 
   @Input() service: EntityService<T, any>;
   @Input() name: string;
-  @Input() fields: { key: string, title: string, type?: string, required?:boolean }[];
+  @Input() fields: { key: string, title: string, type?: string, required?: boolean }[];
   @Input() dialogRef: MatDialogRef<any>;
   @Input() data: any;
+  @Input() preProcessing?: Observable<any>
 
   entity: T;
   disabled: boolean;
@@ -30,12 +32,14 @@ export class AbstractFormComponent<T extends Entity> implements OnInit, AfterVie
     private _snackBar: MatSnackBar, private dialog: MatDialog) { }
 
   ngAfterViewInit(): void {
-    if (!this.tokenService.hasRole('writer') && !this.tokenService.hasRole('admin')) {
-      this.disable();
-    }
-    if (this.data.entity?.id) {
-      this.service.getOne(this.data.entity.id).subscribe({
-        next: data => {
+    if (!this.preProcessing) this.preProcessing = of(null);
+    let ob$ = this.preProcessing.pipe(concatMap(data => {
+      if (!this.tokenService.hasRole('writer') && !this.tokenService.hasRole('admin')) {
+        this.disable();
+      }
+      if (this.data.entity?.id) {
+        this.form.controls.id.disable();
+        return this.service.getOne(this.data.entity.id).pipe(map(data => {
           this.entity = data;
           this.form.patchValue(this.entity)
           if (this.entity.locked_at) {
@@ -47,25 +51,27 @@ export class AbstractFormComponent<T extends Entity> implements OnInit, AfterVie
             })
           }
         }
-      })
-    }
-    else {
-      /*this.entity = {
-        label: ''
-      };*/
-      this.fields = this.fields.filter(e => e.key !== 'id')
-      this.form.patchValue(this.entity)
-    }
+        ))
+      } else {
+        /*this.entity = {
+                label: ''
+              };*/
+        this.fields = this.fields.filter(e => e.key !== 'id' || e.type === 'status')
+        this.form.patchValue(this.entity)
+      }
+      return of(null)
+    })).subscribe();
+
   }
 
   ngOnInit(): void {
     let group = {};
     for (let field of this.fields) {
-      group[field.key] = field.required? ['', Validators.required] : ['']
+      group[field.key] = field.required ? ['', Validators.required] : ['']
     }
 
     this.form = this.formBuilder.group(group);
-    this.form.controls.id.disable();
+
   }
 
   disable() {
@@ -74,6 +80,7 @@ export class AbstractFormComponent<T extends Entity> implements OnInit, AfterVie
   }
 
   action() {
+    console.log(this.form.errors)
     if (this.form.invalid) return;
     this.entity = { ...this.entity, ...this.form.getRawValue() }
     if (!this.entity.id) this.entity.id = undefined;
--- output-ui/src/app/pages/windows/status-form/status-form.component.ts
+++ output-ui/src/app/pages/windows/status-form/status-form.component.ts
@@ -1,21 +1,21 @@
-import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
-import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { AuthorizationService } from 'src/app/security/authorization.service';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Observable, map } from 'rxjs';
 import { StatusService } from 'src/app/services/entities/status.service';
-import { ConfirmDialogComponent, ConfirmDialogModel } from 'src/app/tools/confirm-dialog/confirm-dialog.component';
 import { Status } from '../../../../../../output-interfaces/Publication';
+import { AbstractFormComponent } from '../abstract-form/abstract-form.component';
 
-export function createUniqueValidator(statuses:Status[]): ValidatorFn {
-  return (control:AbstractControl) : ValidationErrors | null => {
-      const value = control.value;
-      if (!value) {
-          return null;
-      }
-      let notUnique = statuses.find(e => e.id == value)
-
-      return notUnique? {notUnique:true}: null;
+export function createUniqueValidator(statuses: Status[]): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.get('id').value;
+    if (!value) {
+      return null;
+    }
+    let status_notUnique = statuses.find(e => e.id == value)
+    //pattern non-negative
+    let status_pattern = value < 0;
+    return status_notUnique || status_pattern ? { status_notUnique, status_pattern } : null;
   }
 }
 
@@ -24,94 +24,30 @@ export function createUniqueValidator(statuses:Status[]): ValidatorFn {
   templateUrl: './status-form.component.html',
   styleUrl: './status-form.component.css'
 })
-export class StatusFormComponent implements OnInit, AfterViewInit {
+export class StatusFormComponent implements OnInit {
+  name = "Status"
+  fields = [
+    { key: 'id', title: 'Status', type: 'status', required: true },
+    { key: 'label', title: 'Bezeichnung', required: true },
+    { key: 'description', title: 'Beschreibung' },
+  ]
+  statuses: Status[];
+  @ViewChild(AbstractFormComponent) formC;
 
-  public form: FormGroup;
+  constructor(public dialogRef: MatDialogRef<StatusFormComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any, public service: StatusService) { }
 
-  status:Status;
-  disabled:boolean;
-  statuses:Status[];
-
-  constructor(public dialogRef: MatDialogRef<StatusFormComponent>, public tokenService: AuthorizationService,
-    @Inject(MAT_DIALOG_DATA) public data: any, private formBuilder: FormBuilder, private statusService: StatusService,
-    private _snackBar:MatSnackBar, private dialog:MatDialog) { }
-
-  ngAfterViewInit(): void {
-    if (!this.tokenService.hasRole('writer') && !this.tokenService.hasRole('admin')) {
-      this.disable();
-    }
-    if (this.data.entity?.id !== undefined && this.data.entity?.id !== null) {
-      this.statusService.getOne(this.data.entity.id).subscribe({
-        next: data => {
-          this.status = data;
-          this.form.patchValue(this.status)
-          if (this.status.locked_at) {
-            this.disable();
-            this._snackBar.open('Status wird leider gerade durch einen anderen Nutzer bearbeitet', 'Ok.', {
-              duration: 5000,
-              panelClass: [`warning-snackbar`],
-              verticalPosition: 'top'
-            })
-          }
-          this.form.get('id').disable();
-        }
-      })
-    }
-    else {
-      this.status = {
-        label: ''
-      };
-      this.form.patchValue(this.status)
-    }
-    this.statusService.getAll().subscribe({
-      next: data => {
+  preProcessing(): Observable<any> {
+    return this.service.getAll().pipe(map(
+      data => {
         this.statuses = data;
-        if (this.data.status?.id !== undefined && this.data.status?.id !== null)  this.statuses = this.statuses.filter(e => e.id !== this.data.status.id)
-        this.form.get('id').addValidators(createUniqueValidator(this.statuses))
+        if (this.data.status?.id !== undefined && this.data.status?.id !== null) this.statuses = this.statuses.filter(e => e.id !== this.data.status.id)
+        this.formC.validator = createUniqueValidator(this.statuses)
       }
-    })
+    ))
   }
 
   ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      id: ['', [Validators.required, Validators.pattern("[0-9]*")]],
-      label: ['', Validators.required],
-      description: [''],
-    });
-  }
-
-  disable() {
-    this.disabled = true;
-    this.form.disable();
-  }
-
-  action() {
-    if (this.form.invalid) return;
-    this.status = { ...this.status, ...this.form.getRawValue() }
-    this.dialogRef.close({...this.status, updated: true})
-  }
-
-  close() {
-    this.dialogRef.close(null)
-  }
-
-  abort() {
-    if (this.form.dirty) {
-      let dialogData = new ConfirmDialogModel("Ungesicherte Änderungen", `Es gibt ungespeicherte Änderungen, möchten Sie diese zunächst speichern?`);
-
-      let dialogRef = this.dialog.open(ConfirmDialogComponent, {
-        maxWidth: "400px",
-        data: dialogData
-      });
-
-      dialogRef.afterClosed().subscribe(dialogResult => {
-        if (dialogResult) { //save
-          this.action();
-        } else if (this.status.id) this.dialogRef.close({ id: this.status.id, locked_at: null })
-        else this.close()
-      });
-    } else if (this.status.id) this.dialogRef.close({ id: this.status.id, locked_at: null })
-    else this.close()
   }
 }
 
