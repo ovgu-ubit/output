8a31a7d620c23491da20bd18791809cf51276378
bossanova1986 <sbosse@ovgu.de>
2025-02-18 10:41:44 +0100

    version 1.1.1

--- output-ui/src/app/app.component.ts
+++ output-ui/src/app/app.component.ts
@@ -12,7 +12,7 @@ import { ConfigService } from './services/config.service';
 })
 export class AppComponent implements OnInit, OnDestroy {
   public title: string = 'Output2';
-  public version = '1.1.0';
+  public version = '1.1.1';
   public user: string | null;
   public institution: string = '';
   public isLoading: boolean = true;
