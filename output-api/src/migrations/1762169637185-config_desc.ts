876d1e7b0f38086e60b2e85989d9142d1ce27789
bossanova1986 <sbosse@ovgu.de>
2025-11-03 12:58:58 +0100

    migration fix

--- output-api/src/migrations/1762169637185-config_desc.ts
+++ output-api/src/migrations/1762169637185-config_desc.ts
@@ -5,14 +5,10 @@ export class ConfigDesc1762169637185 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "config" ADD "description" character varying`);
-        await queryRunner.query(`ALTER TABLE "config" DROP COLUMN "value"`);
-        await queryRunner.query(`ALTER TABLE "config" ADD "value" text`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "config" DROP COLUMN "value"`);
-        await queryRunner.query(`ALTER TABLE "config" ADD "value" character varying`);
-        await queryRunner.query(`ALTER TABLE "config" DROP COLUMN "description"`);
     }
 
 }
