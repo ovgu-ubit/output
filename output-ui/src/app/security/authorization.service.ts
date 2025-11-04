e0504d60546b4f50e595ff32980b64f0e0965087
bossanova1986 <106057750+bossanova1986@users.noreply.github.com>
2025-11-04 13:14:56 +0100

    414 security concept add more explanation (#417)
    * styles fix
    
    * Update README.md
    
    * auth service comment
    
    * Update README.md
    
    * Update README.md

--- output-ui/src/app/security/authorization.service.ts
+++ output-ui/src/app/security/authorization.service.ts
@@ -5,30 +5,57 @@ import { Injectable } from "@angular/core";
 })
 export abstract class AuthorizationService {
 
+  /**
+   * 
+   * @returns true if the request comes from an authenticated user
+   */
   isValid(): boolean {
     return true;
   }
 
+  /**
+   * 
+   * @returns the username of the authenticated user or null
+   */
   getUser(): string | null {
     return null;
   }
 
+  /**
+   * 
+   * @returns the permission array of the authenticated user in the form {rolename, appname}
+   */
   getPermissions(): { rolename: string, appname: string | null }[] | null {
     return null;
   }
 
+  /**
+   * 
+   * @param rolename the required rolename for a function
+   * @returns true if the authenticated user has the required role
+   */
   hasRole(rolename: string): boolean {
     return true;
   }
 
+  /**
+   * handles the login if a user is not authenticated
+   * @param state 
+   */
   login(state): void {
 
   }
 
+  /**
+   * handles what happens if the user clicks on Logout
+   */
   logout(): void {
 
   }
 
+  /**
+   * handles what happens if the user clicks on account details
+   */
   details(): void {
     
   }
