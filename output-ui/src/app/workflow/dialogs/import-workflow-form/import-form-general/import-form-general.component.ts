661e0c220af1118226ae457a7cbb6a58828c42cc
bossanova1986 <sbosse@ovgu.de>
2026-02-04 10:42:37 +0100

    strategy

--- output-ui/src/app/workflow/dialogs/import-workflow-form/import-form-general/import-form-general.component.ts
+++ output-ui/src/app/workflow/dialogs/import-workflow-form/import-form-general/import-form-general.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { ImportWorkflow } from '../../../../../../../output-interfaces/Workflow';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ImportFormFacade } from '../import-form-facade.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-import-form-general',
@@ -22,16 +23,15 @@ export class ImportFormGeneralComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      id: [''],
+      id: [{value:'', disabled:true}],
       label: [''],
       version: [''],
-      created_at: [''],
-      modified_at: [''],
-      published_at: [''],
-      deleted_at: [''],
+      created_at: [{value: new Date(), disabled:true}],
+      modified_at: [{value: new Date(), disabled:true}],
+      published_at: [{value: new Date(), disabled:true}],
+      deleted_at: [{value: new Date(), disabled:true}],
       description: [''],
     })
-    this.form.get('id').disable();
     this.facade.import$.forEach(e => {
       this.entity = e;
       this.form.patchValue(e)
--- output-ui/src/app/workflow/dialogs/import-workflow-form/import-form-strategy/import-form-strategy.component.ts
+++ output-ui/src/app/workflow/dialogs/import-workflow-form/import-form-strategy/import-form-strategy.component.ts
@@ -1,11 +1,124 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ImportWorkflow, Strategy } from '../../../../../../../output-interfaces/Workflow';
+import { ImportFormFacade } from '../import-form-facade.service';
+import { MatSelectModule } from '@angular/material/select';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @Component({
   selector: 'app-import-form-strategy',
-  imports: [],
+  imports: [
+    SharedModule
+  ],
   templateUrl: './import-form-strategy.component.html',
   styleUrl: './import-form-strategy.component.css',
 })
-export class ImportFormStrategyComponent {
+export class ImportFormStrategyComponent implements OnInit {
 
+  constructor(private formBuilder: FormBuilder, private facade: ImportFormFacade) { }
+
+  selectionForm: FormGroup;
+  strategyForm: FormGroup;
+
+  entity: ImportWorkflow;
+
+  previousStrategy = Strategy.URL_QUERY_OFFSET;
+
+  strategies = [
+    {value: Strategy.URL_QUERY_OFFSET, label: 'Web-Abfrage per Suche und Offset', id: 'offset'},
+    {value: Strategy.URL_DOI, label: 'Web-Abfrage per DOI', id: 'doi'},
+    {value: Strategy.FILE_XLSX, label: 'Datei-Upload', id: 'file'},
+  ]
+
+  formats = [
+    {value: 'json', label: 'JSON'},
+    {value: 'xml', label: 'XML'},
+    {value: 'csv', label: 'CSV'},
+    {value: 'xlsx', label: 'XLSX'},
+  ]
+
+  ngOnInit(): void {
+    this.selectionForm = this.formBuilder.group({
+      strategy: this.formBuilder.nonNullable.control<Strategy>(Strategy.URL_QUERY_OFFSET),
+    })
+    this.previousStrategy = this.selectionForm.controls.strategy.value;
+    this.strategyForm = this.buildForm(this.previousStrategy);
+
+    this.facade.import$.forEach(e => {
+      if (!e) return;
+      this.entity = e;
+      this.previousStrategy = e.strategy_type;
+      this.selectionForm.controls.strategy.setValue(e.strategy_type, { emitEvent: false })
+      this.applyStrategy(e.strategy_type);
+    })
+
+    this.selectionForm.controls.strategy.valueChanges.subscribe(async (next) => {
+      if (!next || next === this.previousStrategy) return;
+      this.applyStrategy(next);
+        return;
+    })
+  }
+
+  private applyStrategy(next: Strategy) {
+    this.strategyForm = this.buildForm(next);
+    this.previousStrategy = next;
+  }
+
+  get selectedStrategyId(): string {
+    return this.strategies.find(e => e.value === this.selectionForm.controls.strategy.value)?.id;
+  }
+
+  action() {
+
+  }
+
+  getLabel(s:Strategy) {
+    return this.strategies.find(e => e.value === s)
+  }
+
+  private buildForm(key: Strategy): FormGroup {
+    let res;
+    switch (key) {
+      case Strategy.FILE_XLSX:
+        res = this.formBuilder.group({
+          only_import_if_authors_inst: [true],
+          format: ['', Validators.required],
+          exclusion_criteria: ['', Validators.required]
+        });
+        break;
+      case Strategy.URL_DOI:
+        res = this.formBuilder.group({
+          only_import_if_authors_inst: [true],
+          format: ['', Validators.required],
+          exclusion_criteria: ['', Validators.required],
+          delayInMs: [0, [Validators.min(0), Validators.max(60000)]],
+          parallelCalls: [1, [Validators.min(1), Validators.max(20)]],
+          url_doi: ['', Validators.required],
+          get_doi_item: ['', Validators.required],
+        });
+        break;
+      case Strategy.URL_QUERY_OFFSET:
+      default:
+        res = this.formBuilder.group({
+          only_import_if_authors_inst: [true],
+          format: ['', Validators.required],
+          exclusion_criteria: ['', Validators.required],
+          delayInMs: [0, [Validators.min(0), Validators.max(60000)]],
+          parallelCalls: [1, [Validators.min(1), Validators.max(20)]],
+          url_count: ['', Validators.required],
+          url_items: ['', Validators.required],
+          max_res: [100, [Validators.required, Validators.min(1), Validators.max(5000)]],
+          max_res_name: ['', Validators.required],
+          request_mode : ['', Validators.required],
+          offset_name : ['', Validators.required],
+          offset_start : [0, [Validators.min(0)]],
+          offset_count : [0, [Validators.min(0)]],
+          search_text_combiner: ['', Validators.required],
+          get_count: ['', Validators.required],
+          get_items: ['', Validators.required],
+        });
+    }
+    if (this.entity) res.patchValue(this.entity.strategy);
+    return res;
+  }
 }
