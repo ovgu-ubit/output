67a9be6c3360f662cbc3f82ed8fda423bc6c185e
bossanova1986 <sbosse@ovgu.de>
2026-02-04 12:32:36 +0100

    mapping

--- output-ui/src/app/workflow/dialogs/import-workflow-form/import-form-mapping/import-form-mapping.component.ts
+++ output-ui/src/app/workflow/dialogs/import-workflow-form/import-form-mapping/import-form-mapping.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ImportWorkflow } from '../../../../../../../output-interfaces/Workflow';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ImportFormFacade } from '../import-form-facade.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-import-form-mapping',
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './import-form-mapping.component.html',
   styleUrl: './import-form-mapping.component.css',
 })
-export class ImportFormMappingComponent {
+export class ImportFormMappingComponent implements OnInit {
+  readonly workflow$ = this.facade.import$;
+  form: FormGroup;
+
+  constructor(private facade: ImportFormFacade, private formBuilder:FormBuilder) { }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      mapping: [''],
+    });
+    this.facade.import$.subscribe(workflow => {
+      if (!workflow) return;
+      this.form.patchValue({ mapping: workflow.mapping ?? '' }, { emitEvent: false });
+    });
+  }
 
 }
