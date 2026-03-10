47be4d8241b3863835ee2a7c92fedd1d5d9fbcbc
bossanova1986 <sbosse@ovgu.de>
2026-03-10 09:27:18 +0100

    component scope

--- output-api/src/contract/ContractComponent.entity.ts
+++ output-api/src/contract/ContractComponent.entity.ts
@@ -1,7 +1,11 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany, ManyToMany, JoinTable } from "typeorm";
 import { ContractModel, ContractComponent as IContractComponent } from "../../../output-interfaces/Publication"
 import { Contract } from "./Contract.entity";
 import { Invoice } from "../invoice/Invoice.entity";
+import { OA_Category } from "../oa_category/OA_Category.entity";
+import { GreaterEntity } from "../greater_entity/GreaterEntity.entity";
+import { Publication } from "../publication/core/Publication.entity";
+import { PublicationType } from "../pub_type/PublicationType.entity";
 
 @Entity()
 export class ContractComponent implements IContractComponent {
@@ -29,4 +33,16 @@ export class ContractComponent implements IContractComponent {
 
     @OneToMany(() => Invoice, (i) => i.contract_component, { cascade: true })
     pre_invoices?: Invoice[]
+
+    @ManyToMany(() => OA_Category)
+    @JoinTable()
+    oa_categories?: OA_Category[];
+
+    @ManyToMany(() => PublicationType)
+    @JoinTable()
+    pub_types?: PublicationType[];
+
+    @ManyToMany(() => GreaterEntity)
+    @JoinTable()
+    greater_entities?: GreaterEntity[];
 }
